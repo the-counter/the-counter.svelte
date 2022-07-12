8cc7906596cf94f850df2bf14d62780bb8766f5c
YoungPhilGim <youngphil.gim@kasset.co.kr>
2022-07-12 15:34:24 +0900

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Navbar from './lib/Navbar.svelte';
+  import Counter from './lib/Counter.svelte';
+</script>
+
+<Navbar brand={"The Counter"} />
+<div class="contaier">
+  <div class="row justify-content-center">
+    <div class="col-12 col-lg-4">
+      <Counter />
+    </div>
+  </div>
+</div>
+
+<style></style>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,25 @@
+<script>
+  let count = 0;
+  const increment = () => {
+    count = count + 1
+  };
+  const decrement = () => {
+    count = count - 1;
+  };
+</script>
+
+<div class="d-flex align-items-center mt-4">
+  <button
+    class="btn btn-outline-primary"
+    on:click={decrement}
+  >-</button>
+  <p
+    class="mb-0 mx-4"
+  >{count}</p>
+  <button
+    class="btn btn-outline-primary"
+    on:click={increment}
+  >+</button>
+</div>
+
+<style></style>
